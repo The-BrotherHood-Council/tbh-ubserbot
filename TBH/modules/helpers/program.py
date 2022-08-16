14470f016afa8659532a4bff00ed12ab85cde9ec
BROTHERHOOD COUNCIL <109819870+The-BrotherHood-Council@users.noreply.github.com>
2022-08-16 14:12:13 +0530

    Update program.py

--- TBH/modules/helpers/program.py
+++ TBH/modules/helpers/program.py
@@ -1,6 +1,6 @@
 import shlex
-from AdityaHalder.modules.databases.afk_db import get_afk_status
-from AdityaHalder.modules.databases.pmpermit_db import get_approved_users, pm_guard
+from TBH.modules.databases.afk_db import get_afk_status
+from TBH.modules.databases.pmpermit_db import get_approved_users, pm_guard
 from pyrogram import Client
 from pyrogram.types import Message, User
 
