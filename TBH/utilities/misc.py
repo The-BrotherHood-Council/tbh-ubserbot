93212b5d038b0278ce55e20d79bd4d644ec3d0a4
BROTHERHOOD COUNCIL <109819870+The-BrotherHood-Council@users.noreply.github.com>
2022-08-16 14:26:05 +0530

    Update misc.py

--- TBH/utilities/misc.py
+++ TBH/utilities/misc.py
@@ -1,7 +1,7 @@
 import socket
 from pyrogram import filters
-from AdityaHalder import config
-from AdityaHalder.utilities import pymongodb
+from TBH import config
+from TBH.utilities import pymongodb
 from ..logger import LOGGER
 
 SUDOERS = filters.user()
