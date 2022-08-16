8699c4f397e7188be142fc523737b8d8b421af01
BROTHERHOOD COUNCIL <109819870+The-BrotherHood-Council@users.noreply.github.com>
2022-08-16 14:11:04 +0530

    Update filters.py

--- TBH/modules/helpers/filters.py
+++ TBH/modules/helpers/filters.py
@@ -1,6 +1,6 @@
 from typing import Union, List
 from pyrogram import filters
-from AdityaHalder.config import COMMAND_PREFIXES
+from TBH.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
