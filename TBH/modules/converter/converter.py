e13ed2aaaa7a7135499b02e395ce14b3b0269e11
BROTHERHOOD COUNCIL <109819870+The-BrotherHood-Council@users.noreply.github.com>
2022-08-16 13:59:17 +0530

    Update converter.py

--- TBH/modules/converter/converter.py
+++ TBH/modules/converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from os import path
-from AdityaHalder.modules.helpers.errors import FFmpegReturnCodeError
+from TBH.modules.helpers.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
