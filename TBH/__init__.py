686ad19b657f0b72c0d74fbaaf7b50b0d7a03443
BROTHERHOOD COUNCIL <109819870+The-BrotherHood-Council@users.noreply.github.com>
2022-08-17 20:38:29 +0530

    Update __init__.py

--- TBH/__init__.py
+++ TBH/__init__.py
@@ -1,90 +1,162 @@
-import signal
-import sys
+import asyncio
+import os
 import time
+from os import listdir, mkdir
 
 import heroku3
+from aiohttp import ClientSession
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
+from rich.console import Console
+from rich.table import Table
+from motor.motor_asyncio import AsyncIOMotorClient as KaalXD
 
-from .Config import Config
-from .core.logger import logging
-from .core.session import catub
-from .helpers.functions.converter import Convert
-from .helpers.functions.musictool import *
-from .helpers.utils.utils import runasync
-from .sql_helper.globals import addgvar, delgvar, gvarstatus
-
-__version__ = "3.2.0"
-__license__ = "GNU Affero General Public License v3.0"
-__author__ = "TBHUserBot <https://github.com/The-BrotherHood-Council/tbh-ubserbot>"
-__copyright__ = f"TBHUserBot Copyright (C) 2020 - 2021  {__author__}"
-
-catub.version = __version__
-catub.tgbot.version = __version__
-LOGS = logging.getLogger("TBHUserbot")
-bot = catub
-
-StartTime = time.time()
-catversion = "3.2.0"
+from TBH.config import MONGO_DB_URL, LOG_GROUP_ID, OWNER_ID, STRING_SESSION, SUDO_USERS, UPSTREAM_BRANCH, UPSTREAM_REPO
+from TBH.modules.clientbot.clientbot import client, robot, pytgcalls
+from TBH.utilities.misc import sudo
+from TBH.utilities.times import time_to_seconds
+from TBH.utilities.tasks import install_requirements
 
 
-def close_connection(*_):
-    print("Clossing TBH connection.")
-    runasync(catub.disconnect())
-    sys.exit(143)
+loop = asyncio.get_event_loop()
+console = Console()
 
 
-signal.signal(signal.SIGTERM, close_connection)
+### Heroku Shit
+UPSTREAM_BRANCH = UPSTREAM_BRANCH
+UPSTREAM_REPO = UPSTREAM_REPO
 
-UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+### Modules
+MOD_LOAD = []
+MOD_NOLOAD = []
 
-if Config.PRIVATE_GROUP_BOT_API_ID == 0:
-    if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
-        Config.BOTLOG = False
-        Config.BOTLOG_CHATID = "me"
-    else:
-        Config.BOTLOG_CHATID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
-        Config.PRIVATE_GROUP_BOT_API_ID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
-        Config.BOTLOG = True
-else:
-    if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
-        Config.BOTLOG_CHATID = int(f"-{str(Config.PRIVATE_GROUP_BOT_API_ID)}")
-    else:
-        Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
-    Config.BOTLOG = True
+### Mongo DB
+MONGODB_CLI = KaalXD(MONGO_DB_URL)
+db = MONGODB_CLI.Aditya
 
-if Config.PM_LOGGER_GROUP_ID == 0:
-    if gvarstatus("PM_LOGGER_GROUP_ID") is None:
-        Config.PM_LOGGER_GROUP_ID = -100
-    else:
-        Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
-elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
-    Config.PM_LOGGER_GROUP_ID = int(f"-{str(Config.PM_LOGGER_GROUP_ID)}")
+### Sudo Users
+sudo()
 
-try:
-    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
-        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
-            Config.HEROKU_APP_NAME
-        ]
-    else:
-        HEROKU_APP = None
-except Exception:
-    HEROKU_APP = None
+### Boot Time
+boottime = time.time()
+
+### Clients
+aiohttpsession = ClientSession()
+robot = robot
+pytgcalls = pytgcalls
+
+### Config
+SUDOERS = SUDO_USERS
+OWNER_ID = OWNER_ID
+LOG_GROUP_ID = LOG_GROUP_ID
+
+### Bot Info
+BOT_ID = 0
+BOT_NAME = ""
+BOT_USERNAME = ""
+
+### Assistant Info
+ASSIDS = []
+ASSID = 0
+ASSNAME = ""
+ASSUSERNAME = ""
+ASSMENTION = ""
+random_assistant = []
 
 
-# Global Configiables
-COUNT_MSG = 0
-USERS = {}
-COUNT_PM = {}
-LASTMSG = {}
-CMD_HELP = {}
-ISAFK = False
-AFKREASON = None
-CMD_LIST = {}
-SUDO_LIST = {}
-# for later purposes
-INT_PLUG = ""
-LOAD_PLUG = {}
+async def initiate_bot():
+    global SUDOERS, OWNER_ID, ASSIDS
+    global BOT_ID, BOT_NAME, BOT_USERNAME
+    global ASSID, ASSNAME, ASSMENTION, ASSUSERNAME
+    global Heroku_cli, Heroku_app
+    os.system("clear")
+    header = Table(show_header=True, header_style="bold yellow")
+    header.add_column(
+        "TBH Userbot : Best Ever Userbot"
+    )
+    console.print(header)
+    with console.status(
+        "[magenta] TBH UserBot Booting...",
+    ) as status:
+        console.print("┌ [red]Booting Up The Bot Clients...\n")
+        await robot.start()
+        console.print("└ [green]Booted Bot Client")
+        console.print("\n┌ [red]Booting Up The User Client...")
+        if STRING_SESSION != "None":
+            await pytgcalls.start()
+            random_assistant.append(1)
+            console.print("├ [yellow]Booted User Client")
+        
+        console.print("└ [green]All Client Bots Booted Successfully!")
+        if "raw_files" not in listdir():
+            mkdir("raw_files")
+        if "downloads" not in listdir():
+            mkdir("downloads")
+        if "cache" not in listdir():
+            mkdir("cache")
+        if "search" not in listdir():
+            mkdir("search")
+        console.print("\n┌ [red]Loading Clients Information...")
+        getme = await robot.get_me()
+        BOT_ID = getme.id
+        if getme.last_name:
+            BOT_NAME = getme.first_name + " " + getme.last_name
+        else:
+            BOT_NAME = getme.first_name
+        BOT_USERNAME = getme.username
+        if STRING_SESSION != "None":
+            getme = await client.get_me()
+            ASSID = getme.id
+            ASSIDS.append(ASSID)
+            ASSNAME = (
+                f"{getme.first_name} {getme.last_name}"
+                if getme.last_name
+                else getme.first_name
+            )
+            ASSUSERNAME = getme.username
+            ASSMENTION = getme.mention
+        console.print("└ [green]Loaded Clients Information!")
+        try:
+            repo = Repo()
+        except GitCommandError:
+            console.print("┌ [red] Checking Git Updates!")
+            console.print("└ [red]Git Command Error\n")
+            return
+        except InvalidGitRepositoryError:
+            console.print("┌ [red] Checking Git Updates!")
+            repo = Repo.init()
+            if "origin" in repo.remotes:
+                origin = repo.remote("origin")
+            else:
+                origin = repo.create_remote("origin", UPSTREAM_REPO)
+            origin.fetch()
+            repo.create_head(UPSTREAM_BRANCH, origin.refs[UPSTREAM_BRANCH])
+            repo.heads[UPSTREAM_BRANCH].set_tracking_branch(
+                origin.refs[UPSTREAM_BRANCH]
+            )
+            repo.heads[UPSTREAM_BRANCH].checkout(True)
+            try:
+                repo.create_remote("origin", UPSTREAM_REPO)
+            except BaseException:
+                pass
+            nrs = repo.remote("origin")
+            nrs.fetch(UPSTREAM_BRANCH)
+            try:
+                nrs.pull(UPSTREAM_BRANCH)
+            except GitCommandError:
+                repo.git.reset("--hard", "FETCH_HEAD")
+            await install_requirements(
+                "pip3 install --no-cache-dir -r Installer"
+            )
+            console.print("└ [red]Git Client Update Completed\n")
 
-# Variables
-BOTLOG = Config.BOTLOG
-BOTLOG_CHATID = Config.BOTLOG_CHATID
-PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
+
+loop.run_until_complete(initiate_bot())
+
+
+def init_db():
+    global db_mem
+    db_mem = {}
+
+
+init_db()
